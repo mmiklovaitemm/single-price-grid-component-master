21666c4c475d9b234ff7ca2f30bfa31e2141200f
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2024-12-17 17:30:51 +0200

    update on css,html, js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// Atidaro modal langą
 function openModal() {
   document.getElementById("modal").style.display = "flex";
 }
 
-// Uždaro modal langą
 document.getElementById("close-modal").addEventListener("click", function () {
   document.getElementById("modal").style.display = "none";
 });
 
-// Uždaro modal spustelėjus už jo ribų
 window.onclick = function (event) {
   const modal = document.getElementById("modal");
   if (event.target === modal) {
